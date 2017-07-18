ce10f7b1e9919f8b835489faa154a6e08f77d56c
ww3519og@my.southeastmn.edu <ww3519og@my.southeastmn.edu>
2017-07-17 22:37:09 -0500

    Submitting Ex1a1

---
+++ 2733rcarrillo/rcarrillo2733/rmcarrillo2733ex1a1/plants.js
@@ -0,0 +1,17 @@
+﻿/*
+    JavaScript 6th Edition
+    Chapter 1
+    Chapter case
+
+    Tinley Xeriscapes
+    Plants web page
+    Author: Ramon Carrillo
+    Date:
+
+    Filename: plants.js
+*/
+
+// define variables containing image source values
+var blanket = "images/blanket.jpg";
+var bluestem = "images/bluestem.jpg";
+var rugosa = "images/rugosa.jpg";
